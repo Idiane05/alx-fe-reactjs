dd548cedb5b38b0a4bd41ce063a4640226f28376
Ingabire Diane <ingabirediane980@gmail.com>
2025-10-31 14:13:01 +0300

    added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,35 +1,35 @@
-// src/App.jsx
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage';
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <WelcomeMessage />
-      <h1>User Profiles</h1>
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-      <UserProfile 
-        name="Bob" 
-        age="30" 
-        bio="Software engineer who enjoys cooking and traveling" 
-      />
-      <UserProfile 
-        name="Charlie" 
-        age="28" 
-        bio="Passionate about music and outdoor adventures" 
-      />
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-      <Footer />
-    </div>
-  );
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
